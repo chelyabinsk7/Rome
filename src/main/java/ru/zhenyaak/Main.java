6bf91d6dc1756a58c11381d153b6c02be8d53e56
EKAvdeev <19215200@sigma.sbrf.ru>
2021-05-25 18:17:38 +0500

    Update Main.java

--- src/main/java/ru/zhenyaak/Main.java
+++ src/main/java/ru/zhenyaak/Main.java
@@ -1,10 +1,9 @@
 package ru.zhenyaak;
 
-import java.io.FileNotFoundException;
 import ru.zhenyaak.service.Menu;
 
 public class Main {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 		Menu.startMenu();
 		Menu.showMenu();
 	}
